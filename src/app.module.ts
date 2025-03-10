aaaf808e582fb2fb14d766c15b5dc035a00e09b9
Javier Godoy Núñez <javier@oxozon.io>
2025-03-10 12:24:22 -0400

    wip(#2): Connecting to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import {
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
 import { join } from 'path';
@@ -16,7 +17,6 @@ import { CoreModule } from './core/core.module';
 import { MetricsInterceptor } from './core/metrics/metrics.interceptor';
 import { DalModule } from './dal/dal.module';
 import { OrchestratorModule } from './orchestrator/orchestrator.module';
-
 /**
  * The AWS Secrets Manager client.
  */
@@ -26,6 +26,54 @@ const secretsManager = new SecretsManagerClient();
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        /**
+         * If the USE_LOCAL_MONGO_DATABASE environment variable is set to true, use the local MongoDB database.
+         */
+        if (configService.get<string>('USE_LOCAL_MONGO_DATABASE') === 'true') {
+          const host = configService.get<string>('DB_HOST');
+          const username = configService.get<string>('DB_USERNAME');
+          const password = configService.get<string>('DB_PASSWORD');
+          const database = configService.get<string>('DB_DATABASE');
+          const encodedUsername = encodeURIComponent(username!);
+          const encodedPassword = encodeURIComponent(password!);
+          return {
+            uri: `mongodb://${encodedUsername}:${encodedPassword}@${host}:27017/${database}?authSource=admin`,
+          };
+        }
+
+        /**
+         * Retrieve the secret from AWS Secrets Manager.
+         */
+        const secretId = configService.get<string>('DB_CREDENTIALS');
+        const secretsManagerClient = new SecretsManagerClient({
+          region: configService.get<string>('AWS_REGION') || 'us-east-1',
+        });
+
+        const secretResponse = await secretsManagerClient.send(
+          new GetSecretValueCommand({ SecretId: secretId }),
+        );
+
+        if (secretResponse.SecretString) {
+          const { host, username, password } = JSON.parse(
+            secretResponse.SecretString,
+          );
+          const database = configService.get<string>('DB_DATABASE');
+          const encodedUsername = encodeURIComponent(username);
+          const encodedPassword = encodeURIComponent(password);
+          return {
+            uri: `mongodb://${encodedUsername}:${encodedPassword}@${host}:27017/${database}?authSource=admin`,
+          };
+        }
+
+        throw new Error(
+          'Failed to retrieve database credentials from AWS Secrets Manager.',
+        );
+      },
+    }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
